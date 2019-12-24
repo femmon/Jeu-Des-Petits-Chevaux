2c130ad1fb55be592652d4176a3a46694f7d072e
Quang Tran <tminhquang00@gmail.com>
2019-12-24 23:28:38 +0700

    Update Nest class

--- src/model/Nest.java
+++ src/model/Nest.java
@@ -1,4 +1,52 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Nest {
+
+    private ArrayList<Horse> horseInNest = new ArrayList<>();
+    private Color color;
+
+    public ArrayList<Horse> getHorseInNest() {
+        return horseInNest;
+    }
+
+    public void setHorseInNest(ArrayList<Horse> horseInNest) {
+        this.horseInNest = horseInNest;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public boolean isEmpty() {
+        if (horseInNest.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void add(Horse horse) {
+        horseInNest.add(horse);
+    }
+
+    public int search(Horse horse) {
+        for (int i = 0; i < horseInNest.size(); i++) {
+            if (horse == horseInNest.get(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void remove(int i) {
+        horseInNest.remove(i);
+    }
+
+
 }
