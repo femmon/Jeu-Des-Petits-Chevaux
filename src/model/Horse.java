a0fc1280fce426d2dbe5590e7e74776fe2000732
femmon <49613439+femmon@users.noreply.github.com>
2019-12-25 22:42:43 +0700

    Implement Horse

--- src/model/Horse.java
+++ src/model/Horse.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Horse {
+    private Color color;
+    private int id;
+
+    public Horse(Color color, int id) {
+        this.color = color;
+        this.id = id;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
