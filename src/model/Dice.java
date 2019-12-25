7eedd57b27f5d30b56d9078c6ddb7399544b4f44
femmon <49613439+femmon@users.noreply.github.com>
2019-12-25 12:01:48 +0700

    Implement Dice

--- src/model/Dice.java
+++ src/model/Dice.java
@@ -1,4 +1,27 @@
 package model;
 
+import java.util.Random;
+
 public class Dice {
+    private int dice1Value;
+    private int dice2Value;
+    private Random numberGenerator;
+
+    public Dice() {
+        numberGenerator = new Random();
+        throwDice();
+    }
+
+    public int getDice1Value() {
+        return dice1Value;
+    }
+
+    public int getDice2Value() {
+        return dice2Value;
+    }
+
+    public void throwDice() {
+        dice1Value = numberGenerator.nextInt(6) + 1;
+        dice2Value = numberGenerator.nextInt(6) + 1;
+    }
 }
