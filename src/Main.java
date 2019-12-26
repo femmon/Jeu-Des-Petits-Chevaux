38fc331865a97d5ab27eb499bce797ed508e3408
femmon <49613439+femmon@users.noreply.github.com>
2019-12-26 15:19:06 +0700

    Create simple cli for the board

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,24 @@ import javafx.stage.Stage;
 import model.*;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static model.Color.*;
 
 public class Main extends Application {
 
+    /**
+     * Posible command:
+     *   "E": exit
+     *   "R": summon a RED horse
+     *   "B": summon a BLUE horse
+     *   "Y": summon a YELLOW horse
+     *   "G": summon a GREEN horse
+     *   "C i m": `C` is one of the 4 initial of color in uppercase (R, B, Y, G),
+     *   `i` is the id of horse: 0 - 3, `m` is the number of steps to move
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-
         ArrayList<Player> a = new ArrayList<>();
         Color[] colors = {RED, BLUE, YELLOW, GREEN};
         for (int i = 0; i < 4; i++) {
@@ -28,21 +34,61 @@ public class Main extends Application {
         }
         Board b = new Board(a);
         b.printState();
-        b.summon(RED);
-        b.summon(BLUE);
-        b.printState();
-        b.move(RED, 0, 5);
-        b.printState();
-        b.move(RED, 0, 5);
-        b.printState();
-        for (int i = 0; i < 9; i++) {
-            b.move(RED, 0, 6);
-            b.printState();
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String command = input.nextLine();
+            String[] args = command.split(" ");
+
+            if (args.length == 1){
+                switch (args[1]) {
+                    case "E":
+                        return;
+                    case "R":
+                        b.summon(RED);
+                        break;
+                    case "G":
+                        b.summon(GREEN);
+                        break;
+                    case "B":
+                        b.summon(BLUE);
+                        break;
+                    case "Y":
+                        b.summon(YELLOW);
+                        break;
+                    default:
+                        continue;
+                }
+                b.printState();
+            } else if (args.length == 3){
+                Color color;
+                switch (args[0]) {
+                    case "R":
+                        color = RED;
+                        break;
+                    case "G":
+                        color = GREEN;
+                        break;
+                    case "B":
+                        color = BLUE;
+                        break;
+                    case "Y":
+                        color = YELLOW;
+                        break;
+                    default:
+                        continue;
+                }
+
+                int id = Integer.parseInt(args[1]);
+                if (id < 0 || id > 3) continue;
+
+                int move = Integer.parseInt(args[2]);
+                if (move < 1 || move > 6) continue;
+
+                b.move(color, id, move);
+
+                b.printState();
             }
-        b.move(RED, 0, 5);
-        b.printState("ignore-nest");
-        b.move(RED, 0, 5);
-        b.printState("ignore-nest");
+        }
     }
 
 
