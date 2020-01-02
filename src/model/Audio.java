5cd3c8f636b430e2e2d0046912b35bd8062fd889
Quang Tran <tminhquang00@gmail.com>
2020-01-02 17:36:21 +0700

    Update Audio.java but not tested

--- src/model/Audio.java
+++ src/model/Audio.java
@@ -1,5 +1,29 @@
 package model;
 
+import javafx.scene.media.Media;
+import javafx.scene.Group;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
+
 public class Audio {
-    
+    private String path = "";
+    private Media media = new Media(new File(path).toURI().toString());
+    private MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+    public Audio(String path) {
+         this.path = path;
+    }
+
+    public void start() {
+        Runnable playMusic = () -> mediaPlayer.play();
+        mediaPlayer.setOnReady(playMusic);
+    }
+
+    public void stop() {
+        Runnable stopMusic = () -> mediaPlayer.stop();
+        mediaPlayer.setOnEndOfMedia(stopMusic);
+    }
 }
