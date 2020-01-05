ad38b475cc2756d22b6c34ec4d227eaf1548b337
TranThienVan <59046664+TranThienVan@users.noreply.github.com>
2020-01-05 15:55:45 +0700

    Adding some javafx imports

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
