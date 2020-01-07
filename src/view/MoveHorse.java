866c5f3486af7b07755a76098a151709f6d1caa8
ntduc <56991569+duc2910hp99@users.noreply.github.com>
2020-01-07 12:38:38 +0700

    Add files via upload

---
+++ src/view/MoveHorse.java
@@ -0,0 +1,41 @@
+package santa_claus;
+
+import javafx.animation.FadeTransition;
+import javafx.geometry.Pos;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+class MoveHorse extends StackPane {
+    private ImageView horse;
+    //private int clickCount = 0;
+
+    MoveHorse(ImageView horse) {
+        this.horse = horse;
+
+        /*setOnMouseClicked(event -> {
+            if (clickCount % 2 == 0) {
+                fadeOut();
+            } else {
+                fadeIn();
+            }
+        });*/
+
+        getChildren().add(horse);
+        setAlignment(Pos.CENTER);
+    }
+
+    void fadeIn() {
+        FadeTransition fade = new FadeTransition(Duration.seconds(0.7), horse);
+        fade.setToValue(1);
+        fade.play();
+        //clickCount++;
+    }
+
+    void fadeOut() {
+        FadeTransition fade = new FadeTransition(Duration.seconds(0.7), horse);
+        fade.setToValue(0);
+        fade.play();
+        //clickCount++;
+    }
+}
