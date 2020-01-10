b1a689b5f753f6f36f96d0748a4834d9e5f88f5c
CallmeHongmaybe <44527246+CallmeHongmaybe@users.noreply.github.com>
2020-01-10 20:20:49 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,7 @@ public class Main extends Application {
      // there is ONLY one controller, the controller instance
      @Override public void start(Stage primaryStage) throws IOException { GameController.getInstance().update(); }
 
-    public static void main(String[] args) {
-        launch(args);
-    } // reincarnation of fps
+     public static void main(String[] args) { launch(args);} // reincarnation of fps
 
 }
 
