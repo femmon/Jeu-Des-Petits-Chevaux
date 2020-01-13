e524d14b8dd95a22ed791b79663576ab2c79c00e
femmon <49613439+femmon@users.noreply.github.com>
2020-01-13 19:11:11 +0700

    Fix missing import

--- src/view/settingController.java
+++ src/view/settingController.java
@@ -4,14 +4,17 @@ import controller.GameController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
